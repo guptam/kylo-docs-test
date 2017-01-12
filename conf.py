1115341c5a39ec1300b0b7da85114690428cbc80
Jeremy Merrifield <jeremy.merrifield@thinkbiganalytics.com>
2017-01-12 14:25:17 -0800

    updates to the index file

--- conf.py
+++ conf.py
@@ -16,8 +16,8 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+ import os
+ import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
 
@@ -154,5 +154,9 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
-
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
--- index.rst
+++ index.rst
@@ -3,14 +3,8 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to Kylo Operations Guide's documentation!
+Welcome to the Kylo documentation!
 =================================================
-
-.. toctree::
-   :maxdepth: 2
-   :caption: Contents:
-
-   KyloOperationsGuide.rst
 
 Indices and tables
 ==================
@@ -18,3 +12,11 @@ Indices and tables
 * :ref:`genindex`
 * :ref:`modindex`
 * :ref:`search`
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   KyloOperationsGuide.rst
+
